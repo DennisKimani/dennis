929a2a85888a0aeb445c82db3acd9f2849da43b4
DennisKimani <dennisngugi.dk@gmail.com>
2017-09-14 10:38:32 +0300

    added CRUD functionality

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,50 @@
 class LinksController < ApplicationController
+  def index
+    @links = Link.all
+    render :index
+  end
+
+  def show
+    @link = Link.find(params[:id])
+    render :show
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      redirect_to links_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect_to links_path
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:name, :description, :github_link, :launch)
+  end
 end
