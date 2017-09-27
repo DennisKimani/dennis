8015831d5ab378344f5187a5cc1e0ca0bcf30065
DennisKimani <dennisngugi.dk@gmail.com>
2017-09-27 11:05:15 +0300

    created the extra row in the links table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914064537) do
+ActiveRecord::Schema.define(version: 20170927080315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170914064537) do
     t.string "launch"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
