cf327574027aa3f2742973e8965ee9e6e3c9a7d3
DennisKimani <dennisngugi.dk@gmail.com>
2017-10-14 15:20:31 +0300

    routed the page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :homes
   resources :links
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/skills',to: 'homes#skill', via: :get
 end
